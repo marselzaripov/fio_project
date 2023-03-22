986eb0b3fe4f5dbbb9e2c7ac517bc444951ae05a
Mars Developer <developermars0@gmail.com>
2023-03-22 09:47:30 +0300

    to do added

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
 # fio_project
+
+MANIFEST
+We, the participants of this supranational system, announce a manifesto that will serve as the basis for the creation of a new system of governance based on the principles of equality and justice. Our manifesto consists of the following items:
+1. We take responsibility for the destruction of the categories of nationality, religion and citizenship, which are the reason for the distribution of power and resources between groups based on the principle of "divide and rule". The participants of the system will renounce these categories by renouncing citizenship, renouncing religion and nationality.
+2. Each participant will have the opportunity to put forward bills and amendments, as well as vote for them. This will allow us to create a management system that will reflect the opinions and needs of all participants.
+3. The distribution of the budget will be carried out by voting of the participants. This ensures that the budget will be distributed in such a way as to meet the needs of the majority of participants.
+4. All participants will be required to pay the accepted taxes in accordance with the progressive scale of taxation. This will make it possible to distribute financial resources fairly and ensure equality of chances for all participants in the system.
+5. Each participant will perform a law enforcement function, which will ensure security and order in society.
+6. Representatives (judges), panels of judges and jurors will be selected through a vote of the participants. The powers will be terminated if the majority of votes is lost, which will ensure more responsible behavior of representatives of the judicial system.
+7. All participants of the system will be required to undergo training in the use of the system, the basics of politics, economics, law enforcement, etc. This will help to form a broader outlook among the participants and provide a better understanding of the current management system.
+8. A tax on unused capital will help reduce social inequality and allow resources to be used more efficiently.
+9. Revision of the ideas of the consumer society. Support for environmentally friendly and sustainable production processes and products, as well as reducing resource consumption. Educating the population on environmentally friendly and efficient ways of consumption and production.
+10. The development of an economy based on the value of human life and the well-being of society as a whole, and not only on consumption and accumulation of wealth to the detriment of nature and society.
+
+
+Initializing laws:
+- Access to voting in the system from the age of 21;
+- Access to legislation from 21;
+- Access to be elected judge/jury of the court from the age of 25
+- Compulsory service in law enforcement agencies from the age of 21
+- Deprivation of the weight of the vote for not voting under the laws of those who exceeded the threshold of 10 percent of votes;
+- Deprivation of the weight of the vote for non–compliance with consensus (for example, Proof of Uptime - provision of capacity for the operability of the intra-system Internet/messenger)
+
+TO DO:
+- It is necessary to finalize the Proof of Humanity protocol (https://www.proofofhumanity.org/) to reduce the cost of the registration process and automation.
+- Access to the system after the adoption of initializing laws;
+- Implementation of the mechanism of deprivation of the weight of the vote for not voting under the laws of those who exceeded the threshold of 10 percent of votes;
+- Implementation of the mechanism of deprivation of the weight of the vote for non–compliance with consensus (for example, Proof of Uptime - provision of capacity for the operability of the intra-system Internet/messenger)
+- Creation of a forum for discussing laws and bills (authorization by wallet for system participants, id of the law = id of the forum post)
--- src/Home.jsx
+++ src/Home.jsx
@@ -27,32 +27,23 @@ export default function Home() {
       <p>9. Revision of the ideas of the consumer society. Support for environmentally friendly and sustainable production processes and products, as well as reducing resource consumption. Educating the population on environmentally friendly and efficient ways of consumption and production.
       </p>
       <p>10. The development of an economy based on the value of human life and the well-being of society as a whole, and not only on consumption and accumulation of wealth to the detriment of nature and society.
-      </p>
+      </p>      
 
-      <h1>Манифест</h1>
-      <p>Мы, участники данной надгосударственной системы, объявляем манифест, который будет служить основой для создания новой системы управления, основанной на принципах равенства и справедливости. Наш манифест состоит из следующих пунктов:
-</p>
-      <p>1.	Мы принимаем на себя ответственность за уничтожение категорий национальности, религии и гражданства, являющихся причиной распределения власти и ресурсов между группами на основе принципа «разделяй и властвуй». Участники системы откажутся от данных категорий путем выхода из гражданства, отказа от религии и национальности. Данные категории являются основой для манипуляций над сознанием человека.
-</p>
-      <p>2.	Каждый участник будет иметь возможность выдвигать законопроекты и поправки, а также голосовать за них. Это позволит нам создать систему управления, которая будет отражать мнения и потребности всех участников.
-</p>
-      <p>3.	Распределение бюджета будет осуществляться путем голосования участников. Это гарантирует, что бюджет будет распределен таким образом, чтобы отвечать потребностям большинства участников.
-</p>
-      <p>4.	Все участники будут обязаны уплачивать принятые налоги в соответствии с прогрессивной шкалой налогообложения. Это позволит справедливо распределять финансовые ресурсы и обеспечить равенство шансов для всех участников системы.
-</p>
-      <p>5.	Каждый участник будет осуществлять правоохранительную функцию, что позволит обеспечить безопасность и порядок в обществе.
-</p>
-      <p>6.	Представители (судьи), коллегии судей и присяжные будут выбираться через голосование участников. Полномочия будут прекращаться при утере большинства голосов, что обеспечит более ответственное поведение представителей судебной системы.
-</p>
-      <p>7.	Все участники системы будут обязаны проходить обучение использованию системы, основ политики, экономики, правоохранительной деятельности и т.д. Это поможет сформировать у участников более широкий кругозор и обеспечит лучшее понимание действующей системы управления.
-</p>
-      <p>8.	Налог на неиспользуемый капитал поможет снизить социальное неравенство и позволит использовать ресурсы более эффективнее.
-</p>
-      <p>9.	Пересмотр идей общества потребления. Поддержка экологически чистых и устойчивых производственных процессов и товаров, а также сокращение потребления ресурсов. Обучение населения экологически чистым и эффективным способам потребления и производства. 
-</p>
-      <p>10. Развитие экономики, основанной на ценности человеческой жизни и благополучия общества в целом, а не только на потреблении и накоплении богатства в ущерб природе и обществу.
-</p>
-      
+<h1>Initializing laws:</h1>
+
+<p>- Access to voting in the system from the age of 21;</p>
+<p>- Access to legislation from 21;</p>
+<p>- Access to be elected judge/jury of the court from the age of 25</p>
+<p>- Compulsory service in law enforcement agencies from the age of 21</p>
+<p>- Deprivation of the weight of the vote for not voting under the laws of those who exceeded the threshold of 10 percent of votes;</p>
+<p>- Deprivation of the weight of the vote for non–compliance with consensus (for example, Proof of Uptime - provision of capacity for the operability of the intra-system Internet/messenger)</p>
+
+<h1>TO DO:</h1>
+<p>- It is necessary to finalize the Proof of Humanity protocol (https://www.proofofhumanity.org/) to reduce the cost of the registration process and automation.</p>
+<p>- Access to the system after the adoption of initializing laws;</p>
+<p>- Implementation of the mechanism of deprivation of the weight of the vote for not voting under the laws of those who exceeded the threshold of 10 percent of votes;</p>
+<p>- Implementation of the mechanism of deprivation of the weight of the vote for non–compliance with consensus (for example, Proof of Uptime - provision of capacity for the operability of the intra-system Internet/messenger);</p>
+<p>- Creation of a forum for discussing laws and bills (authorization by wallet for system participants, id of the law = id of the forum post);</p>
 
       </Container>
   )
